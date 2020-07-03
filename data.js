70f11887c7f28428716bf4ad5519bae8d1873c8c
Michael Neuhold <michaelneuhold@Michaels-MacBook-Pro.local>
2020-07-03 21:33:40 +0200

    data file added

---
+++ data.js
@@ -0,0 +1,91 @@
+data = {
+  news: [
+    {
+      title: "Semistrukturierte Daten in relationalen Datenbanksystemen",
+      subTitle: "Bachelorarbeit - Thema",
+      content: "Durch die gegenwärtig rasant wachsende Zahl an Mobil- und Web-Applikationen spielen semistrukturierte Daten eine immer größere Rolle. Der klassische Einsatz ist dabei der \"einfache Datenaustausch\". Relationale Datenbanksysteme sind im Gegensatz auf eine hoch strukturierte Form der Datenspeicherung ausgerichtet. Um die Vorteile dieser Datenbanksysteme und den dort etablierten Möglichkeiten (zB hochperformante Anfragen, Data Warehousing) auch für semistrukturierte Daten nutzen zu können, bieten bekannte Datenbanken wie Oracle DB und MS SQL Server die Möglichkeit semistrukturierte Daten in relationalen Schemen abzubilden.</br></br>\n\nZiel meiner BA ist es die Integrationsmöglichkeiten von JSON formatierten Daten in den relationalen Datenbanksystemen von Microsoft und Oracle zu untersuchen. Eingebettete Funktionsumfänge, das Zusammenspiel mit Daten in Relationen, Performance- und Implementierungsunterschiede sollen aufgezeigt und mit Beispielen hinterlegt werden."
+    }
+  ],
+  quote: "\"Everybody in this country should learn to program a computer, because it teaches you how to think\" (Steve Jobs)",
+  education: [
+    {
+      status: "ACTIVE",
+      name: "FH OÖ, Campus Hagenberg",
+      meta1: "Software Engineering, Bachelor",
+      meta2: "Okt. 2018 - heute"
+    },
+    {
+      status: "DONE",
+      statusText: "5 YEARS",
+      name: "HTL Hollabrunn",
+      meta1: "Elektronik und technische Informatik",
+      meta2: "Sept. 2012 - Jun. 2017"
+    },
+    {
+      status: "DONE",
+      statusText: "4 YEARS",
+      name: "Gymnasium Stockerau",
+      meta1: "BG/BRG - Informatik",
+      meta2: "Sept. 2008 - Jun. 2012"
+    }
+  ],
+  workExperience: {
+    current: [
+      {
+        status: "ACTIVE",
+        name: "FH OÖ, Campus Hagenberg",
+        meta1: "Tutor",
+        meta2: "März 2020 - Jul. 2020",
+        content: "Betreuung und Unterstütung anderer Studenten im Gegenstand Software Projekt-Engineering"
+      },
+      {
+        status: "ACTIVE",
+        name: "FH OÖ, Campus Hagenberg",
+        meta1: "ÖH STV SE",
+        meta2: "Jul. 2019 - heute",
+        content: "Studiengangsvertreter Software Engineering, Ansprechpartner für Studenten, Organisation von Events"
+      },
+      {
+        status: "DONE",
+        statusText: "2 MONTHS",
+        name: "Siemens",
+        meta1: "Intern",
+        meta2: "Aug. 2019 - Sept. 2019",
+        content: "Praktikant im Bereich Corporate Technology, Entwicklung einer Web-Applikation mit React JS"
+      },
+      {
+        status: "DONE",
+        statusText: "4 MONTHS",
+        name: "Web&Söhne",
+        meta1: "Intern",
+        meta2: "Apr. 2018 - Jul. 2018",
+        content: "Praktikant im Bereich Web-Development, Entwicklung einer Analyse-Applikation für Wordpress-Seiten"
+      },
+      {
+        status: "DONE",
+        statusText: "9 MONTHS",
+        name: "LKH Korneuburg",
+        meta1: "Zivildiener",
+        meta2: "Jul. 2017 - März 2018",
+        content: "Zivildienst im Landesklinikum Korneuburg, Schackl für alles..."
+      }
+    ],
+    older: [
+      {
+        name: "Cimbria Heid",
+        meta1: "INTERN",
+        meta2: "Juli 2016"
+      },
+      {
+        name: "Cimbria Heid",
+        meta1: "INTERN",
+        meta2: "Juli 2015"
+      },
+      {
+        name: "Cimbria Heid",
+        meta1: "INTERN",
+        meta2: "Juli 2014"
+      }
+    ]
+  }
+}
